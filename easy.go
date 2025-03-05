152ef989f4971e0e17ce57305541b23719936230
CarlosLaraFP <carloslarafp@gmail.com>
2025-03-05 03:47:35 -0800

    RomanToInt

--- easy.go
+++ easy.go
@@ -58,3 +58,48 @@ func IsValid(s string) bool {
 	// Stack should be empty if all brackets matched correctly
 	return len(stack) == 0
 }
+
+/*
+Given a roman numeral, convert it to an integer.
+For example, 2 is written as II in Roman numeral, just two ones added together.
+12 is written as XII, which is simply X + II.
+The number 27 is written as XXVII, which is XX + V + II
+
+Roman numerals are usually written largest to smallest from left to right.
+However, the numeral for four is not IIII. Instead, the number four is written as IV.
+Because the one is before the five we subtract it making four.
+The same principle applies to the number nine, which is written as IX.
+There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9 (2 extra in the naive sum in both cases).
+X can be placed before L (50) and C (100) to make 40 and 90 (20 extra in the naive sum in both cases).
+C can be placed before D (500) and M (1000) to make 400 and 900 (200 extra in the naive sum in both cases).
+
+Time complexity: O(N)
+Space complexity: O(1)
+*/
+func RomanToInt(s string) int {
+	dict := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	sum := 0
+
+	for i, currentChar := range s {
+		sum += dict[currentChar]
+
+		if i > 0 {
+			if previousChar := rune(s[i-1]); dict[previousChar] < dict[currentChar] {
+				sum -= dict[previousChar] * 2
+			}
+		}
+	}
+
+	return sum
+}
--- easy_test.go
+++ easy_test.go
@@ -20,3 +20,14 @@ func TestIsValid(t *testing.T) {
 	assert.Equal(t, false, IsValid("{([]})"))
 	assert.Equal(t, false, IsValid("((("))
 }
+
+func TestRomanToInt(t *testing.T) {
+	assert.Equal(t, 3, RomanToInt("III"))
+	assert.Equal(t, 4, RomanToInt("IV"))
+	assert.Equal(t, 5, RomanToInt("V"))
+	assert.Equal(t, 9, RomanToInt("IX"))
+	assert.Equal(t, 40, RomanToInt("XL"))
+	assert.Equal(t, 90, RomanToInt("XC"))
+	assert.Equal(t, 400, RomanToInt("CD"))
+	assert.Equal(t, 900, RomanToInt("CM"))
+}
