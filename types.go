9d21a5dbb6975b5f54585bb367fbd0aa16201eb6
CarlosLaraFP <carloslarafp@gmail.com>
2025-03-08 17:07:20 -0800

    type bounds

--- types.go
+++ types.go
@@ -16,10 +16,28 @@ type Numeric interface {
 	constraints.Integer | constraints.Float
 }
 
+// Linked extends the behavior of linked list types
+type Linked interface {
+	GetHead() any // TODO
+	GetTail() any // TODO
+}
+
 // ListNode is a generic type
 type ListNode[T cmp.Ordered] struct {
 	Val  T
 	Next *ListNode[T]
+}
+
+// Index (generic function) returns the index of x in s, or -1 if not found.
+func Index[T comparable](s []T, x T) int {
+	for i, v := range s {
+		// v and x are type T, which has the comparable
+		// constraint, so we can use == here.
+		if v == x {
+			return i
+		}
+	}
+	return -1
 }
 
 // ErrNegativeSqrt handles negative square root errors
